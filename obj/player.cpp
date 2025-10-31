df79d203d5ccd43192694b86de5c8f873b241fdb
unknown <ivan99.abraham@gmail.com>
2025-10-31 12:08:57 -0300

    Created basic code for player

--- obj/player.cpp
+++ obj/player.cpp
@@ -1,15 +1,31 @@
 #include "player.h"
 
-void InitPlayer()
+void InitPlayer(Vector2 screenSize)
 {
 
-
+	Bird.position.y = screenSize.y *0.2;
+	Bird.position.x = screenSize.x * 0.4;
+	Bird.size.x = screenSize.x * 0.10;
+	Bird.size.y = screenSize.y * 0.10;
+	Bird.speed = 100;
 
 }
 
-void UpdatePlayer()
+void UpdatePlayer(player P1)
 {
 
+	if (IsKeyDown(KEY_W))
+	{
 
+		P1.position.y = P1.position.y - P1.speed * GetFrameTime();
+
+	}
+
+	if (IsKeyDown(KEY_S))
+	{
+
+		P1.position.y = P1.position.y + P1.speed * GetFrameTime();
+
+	}
 
 }

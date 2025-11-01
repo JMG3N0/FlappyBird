ec6a2ea495b316c6e52aa0f3efd4ec3449c98ab4
IvanAbraham <ivan99.abraham@gmail.com>
2025-11-01 11:29:10 -0300

    Corrected player position

--- src/player.cpp
+++ src/player.cpp
@@ -4,8 +4,8 @@ void InitPlayer(Vector2 screenSize, player &bird)
 {
 	bird.size.x = screenSize.x * 0.10;
 	bird.size.y = screenSize.y * 0.10;
-	bird.position.y = screenSize.y *0.2;
-	bird.position.x = screenSize.x * 0.4;
+	bird.position.y = screenSize.y *0.5 - bird.size.y / 2;
+	bird.position.x = screenSize.x * 0.25 - bird.size.x;
 	bird.speed = 400;
 
 }

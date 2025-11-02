1ed1d4fe8b2b8c9c1ad4943c2969a5519931af1f
IvanAbraham <ivan99.abraham@gmail.com>
2025-11-02 15:12:52 -0300

    Added a game over screen to either restart or quit the game after you crash

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,10 @@
 #include "game.h"
 
+bool gameOver = false;
+
 void InitGame(Vector2 screenSize, player& bird, obstacle& pipe1, obstacle& pipe2)
 {
-
+	gameOver = false;
 	InitPlayer(screenSize, bird);
 	InitObstacles(screenSize, pipe1, pipe2);
 	
@@ -11,13 +13,27 @@ void InitGame(Vector2 screenSize, player& bird, obstacle& pipe1, obstacle& pipe2
 void UpdateGame(Vector2 screenSize, player& bird, obstacle& pipe1, obstacle& pipe2)
 {
 
+	if (gameOver)
+	{
+
+		if (IsKeyPressed(KEY_SPACE))
+		{
+
+			InitGame(screenSize, bird, pipe1, pipe2);
+
+		}
+		
+		return;
+	
+	}
+
 	UpdatePlayer(bird);
 	UpdateObstacles(screenSize, pipe1, pipe2);
 
 	if (RectangelColition(bird.size, bird.position, pipe1.size, pipe1.position) || RectangelColition(bird.size, bird.position, pipe2.size, pipe2.position))
 	{
 
-		InitGame(screenSize, bird, pipe1, pipe2);
+		gameOver = true;
 
 	}
 
@@ -30,5 +46,16 @@ void DrawGame(Vector2 screenSize, player &bird, obstacle& pipe1, obstacle& pipe2
 
 	DrawPlayer(bird);
 	DrawObstacle(pipe1, pipe2);
+
+	if (gameOver)
+	{
+
+		DrawRectangle(0, 0, screenSize.x, screenSize.y, { 0, 0, 0, 128 });
+
+		const char* overText = "Para empezar denuevo precione espacio\n para salir precione ESC";
+		int textWidth = MeasureText(overText, 30);
+		DrawText(overText, screenSize.x * 0.5 - textWidth * 0.5, screenSize.y * 0.5, 30, RED);
+
+	}
 
 }
--- src/game.h
+++ src/game.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "physics.h"
 
+extern bool gameOver;
+
 void InitGame(Vector2 screensize, player& bird, obstacle& pipe1, obstacle& pipe2);
 void UpdateGame(Vector2 screensize, player& bird, obstacle& pipe1, obstacle& pipe2);
 void DrawGame(Vector2 screenSize, player& bird, obstacle& pipe1, obstacle& pipe2);

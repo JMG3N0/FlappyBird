737f6c95a25ce7a9fd334b7f48aca3a886347407
Joan <joan.rivas.cepeda@gmail.com>
2025-11-21 08:21:23 -0300

    added color cues to the menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -2,19 +2,21 @@
 namespace flappy {
 	void InitMenu(Vector2 screenSize, button& title, button& start, button& credits, button& quit)
 	{
-
+		Vector2 mousePosition = GetMousePosition();
 		float gap = screenSize.y * 0.10f;
 
+
+		
 		title.size.x = screenSize.x * 0.75f;
 		title.size.y = screenSize.x * 0.12f;
 		title.position.x = (screenSize.x / 2.0f) - (title.size.x / 2.0f);
 		title.position.y = gap;
 
-
 		start.size.x = screenSize.x * 0.20f;
-		start.size.y = screenSize.y * 0.10f;
-		start.position.x = (screenSize.x / 2.0f) - (start.size.x / 2.0f);
-		start.position.y = title.position.y + title.size.y + gap;
+			start.size.y = screenSize.y * 0.10f;
+			start.position.x = (screenSize.x / 2.0f) - (start.size.x / 2.0f);
+			start.position.y = title.position.y + title.size.y + gap;
+		
 
 		credits.size.x = screenSize.x * 0.20f;
 		credits.size.y = screenSize.y * 0.10f;
@@ -61,17 +63,51 @@ namespace flappy {
 
 	void DrawMenu(button& title, button& start, button& credits, button& quit)
 	{
-
+		Vector2 mousePosition = GetMousePosition();
 		ClearBackground(RAYWHITE);
 
 		DrawRectangleV(title.position, title.size, BLACK);
 		DrawText("Flappy Bird", static_cast<int>(title.position.x + 30.0f), static_cast<int>(title.position.y), static_cast<int>(title.size.y), WHITE);
+
+
 		DrawRectangleV(start.position, start.size, BLACK);
-		DrawText("Play", static_cast<int>(start.position.x + 30.0f), static_cast<int>(start.position.y), static_cast<int>(start.size.y), WHITE);
+		if (PointRecColition(mousePosition, start.size, start.position) == true)
+		{
+			DrawText("Play", static_cast<int>(start.position.x + 30.0f), static_cast<int>(start.position.y), static_cast<int>(start.size.y), RED);
+		}
+		else if (PointRecColition(mousePosition, start.size, start.position) == false)
+		{
+			DrawText("Play", static_cast<int>(start.position.x + 30.0f), static_cast<int>(start.position.y), static_cast<int>(start.size.y), WHITE);
+		}
 		DrawRectangleV({ credits.position.x - 10.0f, credits.position.y }, { credits.size.x + 50.0f, credits.size.y }, BLACK);
-		DrawText("Credits", static_cast<int>(credits.position.x), static_cast<int>(credits.position.y), static_cast<int>(credits.size.y), WHITE);
+		if (PointRecColition(mousePosition, credits.size, credits.position) == true)
+		{
+			DrawText("Credits", static_cast<int>(credits.position.x), static_cast<int>(credits.position.y), static_cast<int>(credits.size.y), RED);
+		}
+		else if (PointRecColition(mousePosition, credits.size, credits.position) == false)
+		{
+			DrawText("Credits", static_cast<int>(credits.position.x), static_cast<int>(credits.position.y), static_cast<int>(credits.size.y), WHITE);
+		}
 		DrawRectangleV(quit.position, quit.size, BLACK);
-		DrawText("Exit", static_cast<int>(quit.position.x + 30.0f), static_cast<int>(quit.position.y), static_cast<int>(quit.size.y), WHITE);
+		if (PointRecColition(mousePosition, quit.size, quit.position) == true)
+		{
+			DrawText("Exit", static_cast<int>(quit.position.x + 30.0f), static_cast<int>(quit.position.y), static_cast<int>(quit.size.y), RED);
+		}
+		else if (PointRecColition(mousePosition, quit.size, quit.position) == false)
+		{
+			DrawText("Exit", static_cast<int>(quit.position.x + 30.0f), static_cast<int>(quit.position.y), static_cast<int>(quit.size.y), WHITE);
+		}
+
+
+		
+
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
